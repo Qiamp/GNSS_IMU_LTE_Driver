86a1e6bfd0d7c43291fa950734031a6a10250615
Qiamp <jayzhao0115@163.com>
2025-05-12 16:26:45 +0800

    Add mqtt_imu driver

--- data_collect.sh
+++ data_collect.sh
@@ -5,3 +5,4 @@ source ~/GNSS_IMU_LTE_Driver/devel/setup.bash
 roslaunch imu_gnss_driver imu_gnss_driver.launch & sleep 5
 roslaunch ublox_driver ublox_driver.launch & sleep 5
 roslaunch mqtt_gnss mqtt_gnss.launch & sleep 5
+roslaunch mqtt_imu mqtt_gnss.launch & sleep 5
