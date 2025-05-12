f302de2d557a11c2699c4771ed531a501def4022
Qiamp <jayzhao0115@163.com>
2025-05-12 20:29:28 +0800

    Version 1.1 Add mqtt2ros_imu

--- data_collect.sh
+++ data_collect.sh
@@ -3,6 +3,8 @@ source /opt/ros/noetic/setup.bash
 source ~/GNSS_IMU_LTE_Driver/devel/setup.bash
 
 roslaunch imu_gnss_driver imu_gnss_driver.launch & sleep 5
-roslaunch ublox_driver ublox_driver.launch & sleep 5
-roslaunch mqtt_gnss mqtt_gnss.launch & sleep 5
+# roslaunch ublox_driver ublox_driver.launch & sleep 5
+# roslaunch mqtt_gnss mqtt_gnss.launch & sleep 5
 roslaunch mqtt_imu mqtt_gnss.launch & sleep 5
+roslaunch mqtt_client standalone.launch & sleep 5
+roslaunch mqtt2ros_imu imu.launch
