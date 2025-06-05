54a11e1f4a0e8ad6b6bf2dc4176aa4a58a5de3f2
Qiamp <jayzhao0115@163.com>
2025-06-06 03:18:44 +0800

    Update README and data_collect script

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 install serial package: sudo apt-get install ros-$ROS_DISTRO-serial
+sudo apt install libfmt-dev ros-noetic-paho-mqtt-cpp ros-noetic-paho-mqtt-c
+catkin build
+
+连板卡，用Comtool确定串口号：
+imu_gnss_driver用STM32那个
+gnss_driver用ublox那个
+mqtt_all用at那个
+
+启动以上三个的launch文件即可
+MQTT监听使用MQTTX
+
+接收端安装编mqtt_client和mqtt2ros_all即可
--- data_collect.sh
+++ data_collect.sh
@@ -3,8 +3,9 @@ source /opt/ros/noetic/setup.bash
 source ~/GNSS_IMU_LTE_Driver/devel/setup.bash
 
 roslaunch imu_gnss_driver imu_gnss_driver.launch & sleep 5
-# roslaunch ublox_driver ublox_driver.launch & sleep 5
+roslaunch ublox_driver ublox_driver.launch & sleep 5
 # roslaunch mqtt_gnss mqtt_gnss.launch & sleep 5
-roslaunch mqtt_imu mqtt_gnss.launch & sleep 5
+# roslaunch mqtt_imu mqtt_gnss.launch & sleep 5
+roslaunch mqtt_all mqtt_all.launch & sleep 5
 roslaunch mqtt_client standalone.launch & sleep 5
-roslaunch mqtt2ros_imu imu.launch
+roslaunch mqtt2ros_all imu.launch
